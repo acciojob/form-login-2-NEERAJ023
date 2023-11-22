ec30a395ecb7519591afdef37897278aadbcc5f9
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-22 18:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function submitForm() {
+      // Get form values
+      const firstName = document.getElementById('firstName').value;
+      const lastName = document.getElementById('lastName').value;
+      const phoneNumber = document.getElementById('phoneNumber').value;
+      const email = document.getElementById('email').value;
+
+      // Create the result string
+      const result = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`;
+
+      // Display the result using alert
+      alert(result);
+    }
